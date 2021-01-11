f7798319f722d0cc80194c0aaaee71d3269fbf38
liangyehao@huawei <1094311509@qq.com>
2021-01-12 00:02:34 +0800

    改造成gradle项目 解决依赖类找不到的问题 正则匹配model的格式判断是否真的是model类型

--- src/main/java/com/liang/DvaHelper.java
+++ src/main/java/com/liang/DvaHelper.java
@@ -3,32 +3,28 @@ package com.liang;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.ui.popup.Balloon;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
-import com.intellij.ui.JBColor;
 import org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.SystemIndependent;
 
-import java.awt.*;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
+ * dva助手
+ *
  * @author liangyehao
- * @version 1.0
- * @date 2021/1/9 13:58
- * @content 查看umi项目dva的model
+ * @date 2021/01/11
  */
-public class DvaScanner extends AnAction {
+public class DvaHelper extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
         Project project = anActionEvent.getData(PlatformDataKeys.PROJECT);
@@ -36,8 +32,32 @@ public class DvaScanner extends AnAction {
             String basePath = uniteSeparator(project.getBasePath());
             java.util.List<File> dvaModels = getDvaModels(basePath);
             java.util.List<String> fileNames = getFileNames(dvaModels,basePath);
-            showDvaModels(fileNames);
+            showDvaModels(filterModels(fileNames,basePath));
         }
+    }
+
+    /**
+     * 通过正则表达式过滤出真正的model
+     *
+     * @param fileNames 文件名
+     * @param basePath  基本路径
+     * @return {@link List<String>}
+     */
+    private List<String> filterModels(List<String> fileNames, String basePath) {
+        List<String> list = new ArrayList<>();
+        String isModel = ".+namespace(\\s+)?:(\\s+)?(['\"]).+['\"](\\s+)?,.+";
+        for (String fileName : fileNames) {
+            try {
+                String s = FileUtils.readFileToString(new File(basePath+File.separator+fileName), "UTF-8")
+                        .replaceAll("\\n","");
+                if (Pattern.matches(isModel, s)) {
+                    list.add(fileName);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
     }
 
     private void showDvaModels(java.util.List<String> fileNames) {
@@ -76,3 +96,4 @@ public class DvaScanner extends AnAction {
                 .filter(file -> file.getPath().contains(File.separator + "models") && !file.getPath().contains(".umi")).collect(Collectors.toList());
     }
 }
+
